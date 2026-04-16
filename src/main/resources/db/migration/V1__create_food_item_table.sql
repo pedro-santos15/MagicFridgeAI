f394e427125345c3256681f48d8a56ca1cb82ca8
pedro-santos15 <pedrohssantos1509@gmail.com>
2026-04-16 03:42:48 -0300

    fix: fix integration bugs of flyway migration and JPA

--- src/main/resources/db/migration/V1__create_food_item_table.sql
+++ src/main/resources/db/migration/V1__create_food_item_table.sql
@@ -1,8 +1,8 @@
 --V1: Migration to add table of foods
-CREATE TABLE tb_food_item (
-    id INT PRIMARY KEY,
-    name VARCHAR(100),
-    category VARCHAR(100),
-    quantity INT,
+CREATE TABLE food_item (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    category VARCHAR(100) NOT NULL,
+    quantity INT NOT NULL DEFAULT 0,
     expiration_date DATE
 );
